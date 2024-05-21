de49cdd31b2de8e5c670d01511f0bd1db1f7c5c9
aiXpertLab <aiXpertLab@gmail.com>
2024-05-21 09:18:45 -0400

    update

---
+++ split_pane/src/App.tsx
@@ -0,0 +1,20 @@
+import { FC, useState } from 'react'
+import styles from "./index.module.scss";
+import cn from 'classnames'
+
+const App: FC = () => {
+  const [width, setWidth] = useState(280)
+
+  return (
+      <div className={styles.container}>
+          <div className={cn(styles.leftside, styles.block)} style={{width: `${width}px`}}></div>
+          //left
+          <p>asdfsdsdf</p><p>asdfsdsdf</p><p>asdfsdsdf</p>
+          <div className={cn(styles.rightside, styles.block)} style={{left: `${width}px`}}></div>
+          //right
+          <div className={styles.divider} style={{left: `${width}px`}}/>
+      </div>
+  )
+}
+
+export default App
